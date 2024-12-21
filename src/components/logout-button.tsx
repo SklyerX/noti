71b16e2cd9a91b8e82a359293beb9042e46385f0
Skyler.Ab <abrishami.bardia@gmail.com>
2024-12-21 17:39:23 -0500

    removed imports

--- src/components/logout-button.tsx
+++ src/components/logout-button.tsx
@@ -4,8 +4,6 @@ import { LogOut } from "lucide-react";
 import { DropdownMenuItem } from "./ui/dropdown-menu";
 import { useAction } from "next-safe-action/hooks";
 import { logoutAction } from "./logout-action";
-import { useEffect } from "react";
-import { toast } from "sonner";
 
 export default function LogoutButton() {
   const { execute, status } = useAction(logoutAction);
